6bc220ee42da15f6c82f5d48232fa3a6b2d757c0
Manuel Carrascal <manuel.carrascal@pragma.com.co>
2026-02-10 21:37:56 -0500

    feat: implement circuit breakout and retry pattern

--- infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/nequi/r2dbc/franchise/FranchiseRepositoryAdapter.java
+++ infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/nequi/r2dbc/franchise/FranchiseRepositoryAdapter.java
@@ -4,21 +4,51 @@ import co.com.nequi.model.franchise.Franchise;
 import co.com.nequi.model.franchise.gateways.FranchiseRepository;
 import co.com.nequi.r2dbc.entities.FranchiseEntity;
 import co.com.nequi.r2dbc.helper.ReactiveAdapterOperations;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import org.reactivecommons.utils.ObjectMapper;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Repository
 public class FranchiseRepositoryAdapter extends ReactiveAdapterOperations<Franchise, FranchiseEntity, Long, FranchiseReactiveRepository> implements FranchiseRepository {
+    
     public FranchiseRepositoryAdapter(FranchiseReactiveRepository repository, ObjectMapper mapper) {
         super(repository, mapper, d -> mapper.map(d, Franchise.class));
     }
 
     @Override
+    @CircuitBreaker(name = "database")
+    @Retry(name = "database")
+    @TimeLimiter(name = "database")
     public Mono<Franchise> findByName(String name) {
         return repository.findByName(name)
                 .map(this::toEntity);
     }
 
+    @Override
+    @CircuitBreaker(name = "database")
+    @Retry(name = "database")
+    @TimeLimiter(name = "database")
+    public Mono<Franchise> save(Franchise franchise) {
+        return super.save(franchise);
+    }
 
+    @Override
+    @CircuitBreaker(name = "database")
+    @Retry(name = "database")
+    @TimeLimiter(name = "database")
+    public Mono<Franchise> findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    @CircuitBreaker(name = "database")
+    @Retry(name = "database")
+    @TimeLimiter(name = "database")
+    public Flux<Franchise> findAll() {
+        return super.findAll();
+    }
 }
--- infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/nequi/r2dbc/product/ProductRepositoryAdapter.java
+++ infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/nequi/r2dbc/product/ProductRepositoryAdapter.java
@@ -4,32 +4,68 @@ import co.com.nequi.model.product.Product;
 import co.com.nequi.model.product.gateways.ProductRepository;
 import co.com.nequi.r2dbc.entities.ProductEntity;
 import co.com.nequi.r2dbc.helper.ReactiveAdapterOperations;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import org.reactivecommons.utils.ObjectMapper;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Repository
-public class ProductRepositoryAdapter extends ReactiveAdapterOperations<Product, ProductEntity, Long, ProductReactiveRepository> implements ProductRepository
-{
+public class ProductRepositoryAdapter extends ReactiveAdapterOperations<Product, ProductEntity, Long, ProductReactiveRepository> implements ProductRepository {
+    
     public ProductRepositoryAdapter(ProductReactiveRepository repository, ObjectMapper mapper) {
         super(repository, mapper, d -> mapper.map(d, Product.class));
     }
 
     @Override
+    @CircuitBreaker(name = "database")
+    @Retry(name = "database")
+    @TimeLimiter(name = "database")
     public Mono<Product> findByNameAndBranchId(String name, Long branchId) {
         return repository.findByNameAndBranchId(name, branchId)
                 .map(this::toEntity);
     }
 
     @Override
+    @CircuitBreaker(name = "database")
+    @Retry(name = "database")
+    @TimeLimiter(name = "database")
     public Mono<Void> deleteByIdAndBranchId(Long id, Long branchId) {
         return repository.deleteByIdAndBranchId(id, branchId);
     }
 
     @Override
+    @CircuitBreaker(name = "database")
+    @Retry(name = "database")
+    @TimeLimiter(name = "database")
     public Flux<Product> findByFranchiseId(Long franchiseId) {
         return repository.findByFranchiseId(franchiseId)
                 .map(this::toEntity);
+    }
+
+    @Override
+    @CircuitBreaker(name = "database")
+    @Retry(name = "database")
+    @TimeLimiter(name = "database")
+    public Mono<Product> save(Product product) {
+        return super.save(product);
+    }
+
+    @Override
+    @CircuitBreaker(name = "database")
+    @Retry(name = "database")
+    @TimeLimiter(name = "database")
+    public Mono<Product> findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    @CircuitBreaker(name = "database")
+    @Retry(name = "database")
+    @TimeLimiter(name = "database")
+    public Flux<Product> findAll() {
+        return super.findAll();
     }
 }
